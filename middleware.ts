e6af092dfc81e399d9532a5cf6d048fdbda67586
Wahab Afridi <wahab@ccript.com>
2024-09-09 19:22:41 +0500

    Fixed middlewaren and deployment issue.

--- middleware.ts
+++ middleware.ts
@@ -26,54 +26,8 @@ export async function middleware(req: NextRequest) {
   }
 
   if (isRestrictedRoute && !isLoggedIn)
-    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    return NextResponse.redirect(new URL("/login", nextUrl));
 }
-
-// export default auth(async (req, res) => {
-//   const session = req.auth;
-
-//   const { nextUrl } = req;
-//   const url = req.nextUrl;
-
-//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-//   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
-
-//   const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
-//   const isRestrictedRoute = RESTRICTED_ROUTES.includes(nextUrl.pathname);
-//   const isLoggedIn = !!session?.user;
-
-//   const path = `${url.pathname}`;
-
-//   if (isLoggedIn) {
-//     // Add Authorization header for API routes
-//     const headers = new Headers(req.headers);
-//     headers.set("Authorization", `Bearer ${session.user.accessToken}`);
-//     const response = NextResponse.next({ request: { headers } });
-//     return;
-//   }
-
-//   if (isApiAuthRoute) {
-//     return;
-//   }
-
-//   if (isAuthRoute) {
-//     if (isLoggedIn) {
-//       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-//     }
-//     return;
-//   }
-
-//   if (!isLoggedIn && !isPublicRoute) {
-//     return NextResponse.redirect(new URL("/login", nextUrl));
-//   }
-
-//   if (isLoggedIn && isPublicRoute) {
-//     return;
-//   }
-
-//   if (isRestrictedRoute && !isLoggedIn)
-//     return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-// });
 
 export const config = {
   matcher: [
