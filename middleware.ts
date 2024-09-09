20e3ec2145b94eeaf47e0954e2908e2e029b85f0
Wahab Afridi <wahab@ccript.com>
2024-09-09 19:10:35 +0500

    Deployment issue fixed

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
-import { NextResponse } from "next/server";
-import { auth } from "./app/auth";
-
+import { NextRequest, NextResponse } from "next/server";
+import { getUserCookies } from "./actions/auth.actions";
 const apiAuthPrefix = "/api";
 const DEFAULT_LOGIN_REDIRECT = "/";
 const AUTH_ROUTES = ["/login"];
 const RESTRICTED_ROUTES = ["/"];
 const PUBLIC_ROUTES = [""];
 
-export default auth(async (req, res) => {
-  const session = req.auth;
-
+export async function middleware(req: NextRequest) {
   const { nextUrl } = req;
   const url = req.nextUrl;
 
@@ -18,17 +15,7 @@ export default auth(async (req, res) => {
 
   const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
   const isRestrictedRoute = RESTRICTED_ROUTES.includes(nextUrl.pathname);
-  const isLoggedIn = !!session?.user;
-
-  const path = `${url.pathname}`;
-
-  if (isLoggedIn) {
-    // Add Authorization header for API routes
-    const headers = new Headers(req.headers);
-    headers.set("Authorization", `Bearer ${session.user.accessToken}`);
-    const response = NextResponse.next({ request: { headers } });
-    return;
-  }
+  const isLoggedIn = await getUserCookies();
 
   if (isApiAuthRoute) {
     return;
@@ -51,8 +38,56 @@ export default auth(async (req, res) => {
 
   if (isRestrictedRoute && !isLoggedIn)
     return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-});
+}
+
+// export default auth(async (req, res) => {
+//   const session = req.auth;
+
+//   const { nextUrl } = req;
+//   const url = req.nextUrl;
+
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+//   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+
+//   const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
+//   const isRestrictedRoute = RESTRICTED_ROUTES.includes(nextUrl.pathname);
+//   const isLoggedIn = !!session?.user;
+
+//   const path = `${url.pathname}`;
+
+//   if (isLoggedIn) {
+//     // Add Authorization header for API routes
+//     const headers = new Headers(req.headers);
+//     headers.set("Authorization", `Bearer ${session.user.accessToken}`);
+//     const response = NextResponse.next({ request: { headers } });
+//     return;
+//   }
+
+//   if (isApiAuthRoute) {
+//     return;
+//   }
+
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//     }
+//     return;
+//   }
+
+//   if (!isLoggedIn && !isPublicRoute) {
+//     return NextResponse.redirect(new URL("/login", nextUrl));
+//   }
+
+//   if (isLoggedIn && isPublicRoute) {
+//     return;
+//   }
+
+//   if (isRestrictedRoute && !isLoggedIn)
+//     return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+// });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
